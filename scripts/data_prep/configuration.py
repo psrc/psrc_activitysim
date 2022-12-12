2626ad03c874e0eed34faff228961199566e7d6d
Modeller <Modeller@psrc.local>
2022-12-12 11:02:15 -0800

    Adding revised data prep scripts.

---
+++ scripts/data_prep/configuration.py
@@ -0,0 +1,18 @@
+import argparse
+import os
+
+
+def add_run_args(parser, multiprocess=True):
+    """
+    Run command args
+    """
+    parser.add_argument('-c', '--configs_dir',
+                        type=str,
+                        metavar='PATH',
+                        help='path to configs dir')
+
+
+# if __name__ == '__main__':
+parser = argparse.ArgumentParser()
+add_run_args(parser)
+args = parser.parse_args()
